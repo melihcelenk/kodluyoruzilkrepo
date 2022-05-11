307f83385f32b5121c77d52731b6f0969f50fbc2
Melih Çelenk <melihcelenk@gmail.com>
2022-05-11 17:02:54 +0300

    düzenleme

---
+++ javascript_odev1/js/clock.js
@@ -0,0 +1,40 @@
+
+
+function showTime(){
+
+    const bugun = new Date();
+    let saat = bugun.getHours();
+    let dakika = bugun.getMinutes();
+    let saniye = bugun.getSeconds();
+    let gun = bugun.getDay();
+    let gunAdi;
+    switch (gun){
+        case 1:
+            gunAdi = 'Pazartesi';
+            break;
+        case 2:
+            gunAdi = 'Salı';
+            break;
+        case 3:
+            gunAdi = 'Çarşamba';
+            break;
+        case 4:
+            gunAdi = 'Perşembe';
+            break;
+        case 5:
+            gunAdi = 'Cuma';
+            break;
+        case 6:
+            gunAdi = 'Cumartesi';
+            break;
+        case 0:
+        case 7:
+            gunAdi = 'Pazar';
+            break;
+    }
+
+    document.getElementById('myClock').innerHTML = `${saat}:${dakika}:${saniye} - ${gunAdi}`
+
+    setTimeout(showTime, 1000)
+}
+showTime();
---
+++ javascript_odev1/js/isim.js
@@ -0,0 +1,2 @@
+let isim = prompt("İsminizi giriniz.");
+document.getElementById('myName').innerHTML = isim;
